61a69d947b149450f8ee7e7dfacd6c80ffd33e57
mehmetsukrukavak <mehmetsukru.kavak07@gmail.com>
2019-07-16 23:17:39 +0300

    Dependency Injection

---
+++ ShopApp.DataAccess/Concrete/Memory/MemoryProductDal.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using ShopApp.DataAccess.Abstract;
+using ShopApp.Entities;
+
+namespace ShopApp.DataAccess.Concrete.Memory
+{
+    public class MemoryProductDal : IProductDal
+    {
+        public void Create(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            var products = new List<Product>()
+            {
+                new Product() { Id = 1, Name = "Samsung S8 Plus", ImageUrl = "1.jpg", Price= 3000 },
+                new Product() { Id = 2, Name = "Samsung S9 Plus", ImageUrl = "2.jpg", Price= 6000 },
+                new Product() { Id = 3, Name = "Samsung S10 Plus", ImageUrl = "3.jpg", Price= 8000 },
+
+            };
+
+            return products;
+        }
+
+        public Product GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetOne(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ShopApp.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShopApp.Business.Abstract;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ShopApp.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IProductService _productService;
+        public HomeController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public IActionResult Index()
+        {
+            return View(_productService.GetAll());
+        }
+    }
+}
