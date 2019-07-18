0128b0c91cb60fb6fefd227fe38196ff9cf8e14a
Mehmet Şükrü Kavak <Procenne@DESKTOP-G30EBJK>
2019-07-18 14:52:48 +0300

    minor changes

--- ShopApp.DataAccess/Concrete/EfCore/ShopContext.cs
+++ ShopApp.DataAccess/Concrete/EfCore/ShopContext.cs
@@ -19,6 +19,6 @@ namespace ShopApp.DataAccess.Concrete.EfCore
         }
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
-        public DbSet<ProductCategory> ProductCategories { get; set; }
+       
     }
 }
