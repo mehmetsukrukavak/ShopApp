914ac0d92d788bbe48ccf805de4c72bd3dacb6af
mehmetsukrukavak <mehmetsukru.kavak07@gmail.com>
2019-07-23 19:36:04 +0300

    adding static files

---
+++ ShopApp.WebUI/Middlewares/ApplicaationBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.FileProviders;
+
+namespace ShopApp.WebUI.Middlewares
+{
+    public static class ApplicaationBuilderExtensions
+    {
+        public static IApplicationBuilder CustomStaticFiles(this IApplicationBuilder app)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "node_modules");
+
+            var options = new StaticFileOptions
+            {
+
+                FileProvider = new PhysicalFileProvider(path),
+                RequestPath = "/modules"
+            };
+
+            app.UseStaticFiles(options);
+
+            return app;
+        }
+    }
+}
